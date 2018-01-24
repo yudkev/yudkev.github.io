d0982681a42f3b2721cba9c41ca6d7f1cccbc297
Alina <yudkev@gmail.com>
2018-01-24 15:53:19 -0500

    Add files via upload

---
+++ geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "NEWYORK",
+      'continent': "NA",
+      'country': "US",
+      'region': "NY"
+    },
+    'ip':"144.121.81.1"
+  }]);
+})
+//
+()
+
+;
