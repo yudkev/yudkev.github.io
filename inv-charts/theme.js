a56801574a16ed7b5c395d02cf1581a4f676ea61
Alina <yudkev@gmail.com>
2018-02-08 15:51:53 -0500

    Add files via upload

---
+++ inv-charts/theme.js
@@ -0,0 +1,74 @@
+Highcharts.theme = {
+    //colors: ['#333333','#822f37','#005b9d','#822F37','#7a9ff7','#37BBE4'],
+    chart: {
+        marginTop: 0,
+        spacingTop: 0,
+        marginBottom: 20,
+        plotBackgroundColor: 'transparent',
+        backgroundColor: 'transparent'
+    },
+    title: {
+        text: '',
+    },
+    credits: {
+        enabled: false
+    },
+    xAxis: [{
+        tickLength: 0,
+        lineWidth: 0,
+        minorGridLineWidth: 0,
+        lineColor: 'transparent',
+        minorTickLength: 0,
+        gridLineColor: 'transparent',
+        labels: {
+            style: {
+            fontSize: '10px'
+            }
+        }
+    }],
+    yAxis: [{
+        gridLineColor: 'transparent',
+        labels: {
+            enabled: false,
+        },
+        title: {
+            text: ''
+        }
+    }, {
+        title: {
+            text: ''
+        },
+        labels: {
+            enabled: false
+        }
+    }],
+    tooltip: {
+        formatter: function() {
+            return '<b>' + this.y + 'M</b>';
+        },
+        shadow: false,
+        borderRadius: 0,
+        backgroundColor: '#FFF',
+        padding: 1,
+        style: {},
+        useHTML: true,
+    },
+    legend: {
+        enabled: false
+    },
+    series: [{
+        type: 'column',
+        pointWidth: 35,
+        dataLabels: {
+            enabled: false
+        },
+        tooltip: {
+            valueSuffix: 'm'
+        }
+
+    }]
+};
+
+// Apply the theme
+Highcharts.setOptions(Highcharts.theme);
+
